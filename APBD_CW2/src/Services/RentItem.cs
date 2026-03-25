2f345bf36222eb5ca0fcc43f04217a7890c1b0f2
Arsenii Marushchak <s33304@pjwstk.edu.pl>
2026-03-25 21:49:46 +0100

    Added RentItem class

--- APBD_CW2/src/Services/RentItem.cs
+++ APBD_CW2/src/Services/RentItem.cs
@@ -3,11 +3,30 @@ using APBD_CW2.Models;
 
 namespace APBD_CW2.Services;
 
-public class RentItem
+public class RentItem :IRentItem
 {
     public ItemStatus GetItemStatus(Item item)
     {
         return item.Status;
     }
-  
+
+    public void RentAnItem(Item item, User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReturnAnItem(Item item, User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Item> GetUserRents(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Item> GetItems()
+    {
+        throw new NotImplementedException();
+    }
 }
